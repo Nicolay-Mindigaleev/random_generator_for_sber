e301cb8b71b1cff8d7d2023a2cbbb34d4d577bb6
Николай <mindigaleevkola@gmail.com>
2026-04-10 11:00:41 +0700

    добавлена загрузка из JSON файла

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import random
+import json
 
 #сюды можно добавить нужные лоты с определенным шансом выпадения. Сумма probability должна быть равна 100 
-PRIZES = [
-    {"name": "40%", "probability": 40},
-    {"name": "25%", "probability": 25},
-    {"name": "15%", "probability": 15},
-    {"name": "12%", "probability": 12},
-    {"name": "5%", "probability": 5},
-    {"name": "2%", "probability": 2},
-    {"name": "1%", "probability": 1},
-]
-
+def load_prizes():
+    """Загружает лоты из JSON-файла"""
+    with open('data_base.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data['prizes']
+def check_chance(prize_list: list[dict]):
+    sum = 0
+    for i in prize_list:
+        sum += i["probability"]
+    if abs(sum - 100) > 0.01:
+        raise ValueError("Нарушено условие нормировки")
+     
 def spin_wheel():
+    prizes = load_prizes()
+    check_chance(prizes)
     r = random.randint(1, 100)
-    print(r)
+    print(r) #для отлдаки
     cumulative = 0
-    for prize in PRIZES:
+    for prize in prizes:
         cumulative += prize["probability"]
         if r <= cumulative:
             return prize
-    return PRIZES[-1]
+    return prizes[-1]
 
 result = spin_wheel()
 print(f"Вам выпало: {result['name']}")
